42c029a10d12494ed9844390d7af49b5b5575e1d
Serhii-Buha <serhii.buga@gmail.com>
2023-03-13 13:38:14 +0200

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
 
+let intervalId = null;
+
+const getRandomHexColor = () => {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+const changeBackgroundColor = () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+};
+
+const startBackgroundColorChange = () => {
+  btnStart.disabled = true;
+  intervalId = setInterval(changeBackgroundColor, 1000);
+};
+
+const stopBackgroundColorChange = () => {
+  clearInterval(intervalId);
+  btnStart.disabled = false;
+};
+
+btnStart.addEventListener('click', startBackgroundColorChange);
+btnStop.addEventListener('click', stopBackgroundColorChange);
